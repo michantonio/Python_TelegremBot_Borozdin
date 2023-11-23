cf512e2450de017b23ebc030f9ab3c1bf59b5eb5
michantonio <148625741+michantonio@users.noreply.github.com>
2023-11-23 20:42:50 +0300

    Update README.md

---
+++ draw_board(board).py
@@ -0,0 +1,39 @@
+def draw_board(board):
+    # запустить цикл, который проходит по всем 3 строкам доски
+    for i in range(3):
+        # поставить разделители значений в строке
+        print(" | ".join(board[i]))
+        # поставить разделители строк
+        print("---------")
+
+def ask_and_make_move(player, board):
+    # дать игроку возможность сделать ход, то есть ввести координаты
+    x, y = input(f"{player}, enter x and y coordinates (e.g. 0 0): ").strip().split()
+    # преобразовать координаты в целые числа
+    x, y = int(x), int(y)
+    # задать условие, которое проверяет,
+    # находится ли координата в пределах поля и свободно ли место
+    if (0 <= x <= 2) and (0 <= y <= 2) and (board[x][y] == " "):
+        # если свободно, записать значение игрока (Х или 0) в ячейку
+        board[x][y] = player
+    else:
+        print("That spot is already taken. Try again.")
+        ask_and_make_move(player, board)
+
+def ask_and_make_move(player, board):
+    x, y = ask_move(player, board)
+    # координаты x, y взять из функции ask_move(player, board)
+    make_move(player, board, x, y)
+
+def ask_move(player, board):
+    # дать игроку возможность сделать ход, то есть ввести координаты
+	x, y = input(f"{player}, enter x and y coordinates (e.g. 0 0): ").strip().split()
+    # преобразовать координаты в целые числа
+x, y = int(x), int(y)
+    # задать условие, которое проверяет, свободно ли место
+if (0 <= x <= 2) and (0 <= y <= 2) and (board[x][y] == " "):
+    # если клетка свободна, вернуть её координаты
+return(x, y)
+    else:
+        print("Клетка занята. Введите координаты еще раз.")
+    return ask_move(player, board)

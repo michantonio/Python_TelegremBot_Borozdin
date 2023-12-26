97fa35cfcbbc022044ceaef57f39d53317975b89
mich_antonio <mramorter@mail.ru>
2023-12-26 18:56:26 +0300

    Update README.md

--- draw_board(board).py
+++ draw_board(board).py
@@ -27,13 +27,14 @@ def ask_and_make_move(player, board):
 
 def ask_move(player, board):
     # дать игроку возможность сделать ход, то есть ввести координаты
-	x, y = input(f"{player}, enter x and y coordinates (e.g. 0 0): ").strip().split()
+    x, y = input(f"{player}, enter x and y coordinates (e.g. 0 0): ").strip().split()
     # преобразовать координаты в целые числа
-x, y = int(x), int(y)
+    x, y = int(x), int(y)
     # задать условие, которое проверяет, свободно ли место
-if (0 <= x <= 2) and (0 <= y <= 2) and (board[x][y] == " "):
+    if (0 <= x <= 2) and (0 <= y <= 2) and (board[x][y] == " "):
     # если клетка свободна, вернуть её координаты
-return(x, y)
+        return(x, y)
     else:
         print("Клетка занята. Введите координаты еще раз.")
     return ask_move(player, board)
+
